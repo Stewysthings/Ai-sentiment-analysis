c93566d694dc3f23a16464dc9e2f13c3fcd17f8e
Wendy Stewart <cellardoor@hotmail.ca>
2025-07-11 08:36:56 -0700

    Remove Flask dev server and implement lazy loading for sentiment_analyzer

--- app.py
+++ app.py
@@ -169,6 +169,3 @@ def serve_static(filename):
     """Serves static files from the configured static folder"""
     return send_from_directory(Config.STATIC_FOLDER, filename)
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
